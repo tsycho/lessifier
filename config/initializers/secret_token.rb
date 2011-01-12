8697a964e18c1bc86f460eef3f5302749f4a5197
Asif <sheikh.asif@gmail.com>
2011-01-12 11:05:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lessifier::Application.config.secret_token = '2f37b14a37cbea6eba78b39ec84163dbea8b95d17d096c29c0bca84e740a55fc16bf5383f2a727da40cc95eb027090e9629653ea9d49fcab08cb231e9a891315'
