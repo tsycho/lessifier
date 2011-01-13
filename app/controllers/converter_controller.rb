51a3fec76cb9edd65eeb0f2b4cdc6e818c7b9d4c
Asif <sheikh.asif@gmail.com>
2011-01-12 21:45:15 -0500

    CSS improvements

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,7 +1,16 @@
+require 'net/http'
+
 class ConverterController < ApplicationController
 
   def index    
-    css = params[:css]
+    css = params[:css] || ""
+    if params[:url]
+      begin
+        css = Net::HTTP.get URI.parse(params[:url])
+      rescue
+        @scss = "Unable to load CSS from URL"
+      end
+    end
     l = Lessify.new(:css => css)
     @scss = l.to_scss('tab' => '  ')
   end
